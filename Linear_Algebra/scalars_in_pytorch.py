0f96c846d11d21b5fd06a5c1d59901b34620a6af
GhoshBishwayan <ghoshbishwayan@gmail.com>
2026-01-17 19:51:02 +0530

    2nd commit

--- Linear_Algebra/scalars_in_pytorch.py
+++ Linear_Algebra/scalars_in_pytorch.py
@@ -1,3 +1,10 @@
 import torch  # pip install pytorch
 
-print(torch.__version__)
+# print(torch.__version__)
+
+x_pt = torch.tensor(25) # type specification optional, e.g.: dtype=torch.float16
+
+
+print(x_pt)
+print(x_pt.shape)
+
