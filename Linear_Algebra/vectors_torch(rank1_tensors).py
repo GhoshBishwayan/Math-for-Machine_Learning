14b7fe271d6173cb8fe958078c890de9b8163f00
GhoshBishwayan <ghoshbishwayan@gmail.com>
2026-01-23 13:54:51 +0530

    Vectors using PyTorch (second commit)

--- Linear_Algebra/vectors_torch(rank1_tensors).py
+++ Linear_Algebra/vectors_torch(rank1_tensors).py
@@ -4,10 +4,10 @@ x_pt_pt = torch.tensor([25, 2, 5])
 
 print("Vector: ", x_pt_pt)
 print("Length of the Vector: ", len(x_pt_pt))
-print("Length of the Vector: ", x_pt_pt.shape)
-print("Length of the Vector(using Numpy_pt): ",type(x_pt_pt))
+print("Shape of the Vector: ", x_pt_pt.shape)
+print("Type of the Vector(using PyTorch): ",type(x_pt_pt))
 print("First element of Vector: ",x_pt_pt[0])
-print("Ty_ptpe of each element: ", type(x_pt_pt[0]))
+print("Type of each element: ", type(x_pt_pt[0]))
 
 # Vector Transposition
 
