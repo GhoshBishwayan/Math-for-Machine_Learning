7ffed45aa8a6e18f6f9e8c53193cbc0dadbd6cc2
GhoshBishwayan <ghoshbishwayan@gmail.com>
2026-01-23 13:59:36 +0530

    Vectors using Numpy (second-commit)

--- Linear_Algebra/vectors_numpy(rank1_tensors).py
+++ Linear_Algebra/vectors_numpy(rank1_tensors).py
@@ -4,8 +4,8 @@ x = np.array([25, 2, 5])
 
 print("Vector: ", x)
 print("Length of the Vector: ", len(x))
-print("Length of the Vector: ", x.shape)
-print("Length of the Vector(using Numpy): ",type(x))
+print("Shape of the Vector: ", x.shape)
+print("Type of the Vector(using Numpy): ",type(x))
 print("First element of Vector: ",x[0])
 print("Type of each element: ", type(x[0]))
 
@@ -15,7 +15,7 @@ y = np.array([[25, 2, 5]])
 y_t = y.T
 
 print("Vector before transpose: ", y)
-print("Shape of the Vector before transpose: ", y)
+print("Shape of the Vector before transpose: ", y.shape)
 
-print("Length of the Transpose Vector: ", y_t.shape)
+print("Shape of the Transpose Vector: ", y_t.shape)
 print("The Transpose Vector: ", y_t)
