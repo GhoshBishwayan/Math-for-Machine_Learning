a83111c481f55e95e11e983d4d1432f10298d744
GhoshBishwayan <ghoshbishwayan@gmail.com>
2026-02-04 23:00:31 +0530

    Numpy+Pytorch version

--- Linear_Algebra/Matrix_Multiplication/matrix_mul_with_matrix_inverse.py
+++ Linear_Algebra/Matrix_Multiplication/matrix_mul_with_matrix_inverse.py
@@ -1,4 +1,6 @@
 import numpy as np
+import torch
+
 
 X = np.array([[4, 2], [-5, -3]])
 
@@ -9,3 +11,11 @@ print("Matrix Inversion using Numpy ",Xinv)
 
 print("X.X^-1 = Identity Matrix of length n (using Numpy) ", np.dot(X, Xinv))
 
+
+# using pytorch
+
+X_pt = torch.tensor([[4. , 2.], [-5., -3.]])
+
+Xinv_pt = torch.inverse(X_pt)
+
+print("Matrix Inversion using PyTorch ",Xinv_pt)
