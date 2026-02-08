ff9f4bdaabf2d5dfa63e9d4ed609c4a36f4ee684
GhoshBishwayan <ghoshbishwayan@gmail.com>
2026-02-09 02:39:02 +0530

    first commit

--- Linear_Algebra_two/Matrix_Determinant.py
+++ Linear_Algebra_two/Matrix_Determinant.py
@@ -11,4 +11,16 @@ N = np.array([[-4, 1],[-8, 2]])
 detN = np.linalg.det(N) # 0.0
 
 '''LinAlgError("Singular matrix")'''
-Ninv = np.linalg.inv(N) # This will give singular matrix error
+# Ninv = np.linalg.inv(N) # This will give singular matrix error
+
+
+
+'''Using PyTorch'''
+
+import torch
+
+X_pt = torch.tensor([[4, 2], [-5, -3.]]) #must be float type
+
+detX_pt = torch.det(X_pt)
+
+print(detX_pt)
