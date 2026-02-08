95f4af1796713e8ed58042b7933f5d834f9efbdf
GhoshBishwayan <ghoshbishwayan@gmail.com>
2026-02-09 02:24:01 +0530

    numpy only

---
+++ Linear_Algebra_two/Matrix_Determinant.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+X = np.array([[4, 2], [-5, -3]])
+
+print(np.linalg.det(X))
+
+# Now an example of a matrix with determinant 0 which can't be invertible
+
+N = np.array([[-4, 1],[-8, 2]])
+
+detN = np.linalg.det(N) # 0.0
+
+'''LinAlgError("Singular matrix")'''
+Ninv = np.linalg.inv(N) # This will give singular matrix error
