04082b7c8de5e79dff9e94d48cdffd7fc61e612c
GhoshBishwayan <ghoshbishwayan@gmail.com>
2026-02-18 23:26:00 +0530

    Second Commit

--- advance_topic/Image_compression_via_svd.py
+++ advance_topic/Image_compression_via_svd.py
@@ -2,6 +2,7 @@ import requests
 from PIL import Image
 import matplotlib.pyplot as plt
 from io import BytesIO
+import numpy as np
 
 url = "https://raw.githubusercontent.com/jonkrohn/DLTFpT/master/notebooks/oboe-with-book.jpg"
 
@@ -10,6 +11,13 @@ img = Image.open(BytesIO(response.content))
 
 plt.imshow(img)
 # plt.axis("off")
+
+
+imggray = img.convert('L')
+plt.imshow(imggray)
+# convert image data to numpy matrix
+imgmat = np.array(imggray, dtype=float)
+# plt.imshow(imgmat, cmap='gray')
 plt.show()
 
-imggray = img.convert('LA')
+U, sigma, V = np.linalg.svd(imgmat)
