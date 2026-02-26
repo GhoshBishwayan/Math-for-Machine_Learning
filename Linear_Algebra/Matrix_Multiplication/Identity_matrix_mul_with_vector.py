70a7637fbb4fc70f45f3e64c4bafa65b5ba303fd
GhoshBishwayan <ghoshbishwayan@gmail.com>
2026-02-27 01:06:12 +0530

    Reorganize linear algebra files & folder

--- Linear_Algebra/Matrix_Multiplication/Identity_matrix_mul_with_vector.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-I = torch.tensor([[1,0,0],[0,1,0],[0,0,1]])  #Identity Matrix
-
-x_pt = torch.tensor([25,10,2]) #Vector
-
-print(torch.matmul(I, x_pt))
-
-''' Multiplying Identity Matrix with a Vector will be that Vector '''
--- Linear_Algebra/exercise_segment1.py
+++
@@ -1,6 +0,0 @@
-import torch
-
-x = torch.tensor([[42, 4, 7, 99],[-93, -3, 17, 22]])
-
-#1. Whats the position of 17?
-print(x[1,2])
