d9dd1324b923834749c2fdc19a070f462d2c2a1c
GhoshBishwayan <ghoshbishwayan@gmail.com>
2026-02-26 23:53:30 +0530

    second commit

--- advance_topic/pca.py
+++ advance_topic/pca.py
@@ -23,4 +23,19 @@ print(X.shape) # should reduce no. of features which is column
 print(X[0:6,:])
 
 plt.scatter(X[:, 0], X[:, 1])
+
+
+# Let's differentiate types of flowers here
+
+print(iris.target.shape) # number of features
+print(iris.target[0:6])
+
+import numpy as np
+
+unique_elements, counts_elements = np.unique(iris.target, return_counts=True)
+np.asarray((unique_elements, counts_elements))
+
+print(list(iris.target_names)) # different types of data's name
+
+plt.scatter(X[:, 0], X[:, 1], c=iris.target)
 plt.show()
